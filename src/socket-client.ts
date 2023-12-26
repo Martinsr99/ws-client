0ec83d8a5604741051b699b5b4e31da6a07b9ee7
Martinsr99 <silesreche.martin@gmail.com>
2023-12-26 18:47:40 +0100

    feat: Add broadcast

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,49 +1,64 @@
-import { Manager, Socket } from "socket.io-client"
-
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
+  const manager = new Manager("http://localhost:3001/socket.io/socket.io.js");
 
-    const manager = new Manager('http://localhost:3001/socket.io/socket.io.js')
+  const socket = manager.socket("/");
 
-    const socket = manager.socket('/')
-
-    addListerners(socket)
-}
+  addListerners(socket);
+};
 
 const addListerners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status")!;
+  const clientsUl = document.querySelector("#clients-ul")!;
 
-    const serverStatusLabel = document.querySelector('#server-status')!
-    const clientsUl = document.querySelector('#clients-ul')!
+  const messageForm = document.querySelector<HTMLFormElement>("#message-form")!;
+  const messageInput =
+    document.querySelector<HTMLInputElement>("#message-input")!;
 
-    const messageForm = document.querySelector<HTMLFormElement>('#messageForm')! 
-    const messageInput = document.querySelector<HTMLInputElement>('#messageInput')! 
+  const messagesUl = document.querySelector("#messages-ul")!;
 
-    socket.on('connect', () => {
-        serverStatusLabel.innerHTML = 'connected'
-    })
-    
-    socket.on('disconnect', () => {
-        serverStatusLabel.innerHTML = 'disconnected'
-    })
+  socket.on("connect", () => {
+    serverStatusLabel.innerHTML = "connected";
+  });
 
-    socket.on('clients-updated',(clients: string[]) => {
-        let clientsHtml = ''
-        clients.forEach( clientId => {
-            clientsHtml += `
+  socket.on("disconnect", () => {
+    serverStatusLabel.innerHTML = "disconnected";
+  });
+
+  socket.on("clients-updated", (clients: string[]) => {
+    let clientsHtml = "";
+    clients.forEach((clientId) => {
+      clientsHtml += `
                 <li>${clientId}</li>
-            `
-        })
-        clientsUl.innerHTML = clientsHtml
-    })
-
-    messageForm.addEventListener('submit', (event) => {
-        event.preventDefault()
-        if(messageInput.value.trim.length <= 0) return
-
-        socket.emit('message-from-client', messageInput.value)
-
-        messageInput.value = ''
-    })
+            `;
+    });
+    clientsUl.innerHTML = clientsHtml;
+  });
 
 
-}
+  messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if( messageInput.value.trim().length <= 0 ) return;
+
+    socket.emit('message-from-client', { 
+        id: 'YO!!', 
+        message: messageInput.value 
+    });
+
+    messageInput.value = '';
+});
+
+socket.on('message-from-server', ( payload: { fullName: string, message: string }) => {
+    const newMessage = `
+        <li>
+            <strong>${ payload.fullName }</strong>
+            <span>${ payload.message }</span>
+        </li>
+    `;
+    const li = document.createElement('li');
+    li.innerHTML = newMessage;
+    messagesUl.append( li );
+
+})
+};
