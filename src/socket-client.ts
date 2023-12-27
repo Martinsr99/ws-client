494a2600febef2297e6d9ce8e3303796bbc06305
Martinsr99 <silesreche.martin@gmail.com>
2023-12-27 21:17:42 +0100

    fix: remove old listener when connecting

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,5 +1,7 @@
 import { Manager, Socket } from "socket.io-client";
 
+let socket: Socket;
+
 export const connectToServer = (token: string) => {
   const manager = new Manager("http://localhost:3001/socket.io/socket.io.js", {
     extraHeaders: {
@@ -7,12 +9,13 @@ export const connectToServer = (token: string) => {
     }
   });
 
-  const socket = manager.socket("/");
+  socket?.removeAllListeners()
+  socket = manager.socket("/");
 
-  addListerners(socket);
+  addListerners();
 };
 
-const addListerners = (socket: Socket) => {
+const addListerners = () => {
   const serverStatusLabel = document.querySelector("#server-status")!;
   const clientsUl = document.querySelector("#clients-ul")!;
 
